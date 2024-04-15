738230618c86a00d59cfc6e5eb2631c06fdcbab7
a.morozov <a.morozov@raketa.travel>
2024-04-15 09:35:07 +0500

    some review

--- src/components/products/buttons/ButtonCart.jsx
+++ src/components/products/buttons/ButtonCart.jsx
@@ -1,56 +1,52 @@
 import React from 'react';
 import { addProductCart } from '../../../toolkitRedux/reducers/cartSlice';
-import {
-    useDispatch,
-} from 'react-redux';
+import { useDispatch } from 'react-redux';
+
+const handlerCardProduct = () => (dispatch, getState) => {
+    const {
+        product,
+    } = getState();
+
+    const {
+        nameProduct,
+        selectSize,
+        dataColor,
+    } = product;
+
+    const {
+        name,
+        price,
+        images,
+    } = dataColor;
+
+    if (selectSize === null) {
+        alert('Размер не выбран, выберите размер');
+    } else {
+        let objData = {
+            productName: null,
+            productColor: null,
+            productSize: null,
+            price: null,
+            image: null,
+        };
+
+        objData.productName = nameProduct;
+        objData.productColor = name;
+        objData.price = price;
+        objData.productSize = selectSize;
+        objData.image = images[0];
+
+        dispatch(addProductCart(objData));
+
+        const { cart } = getState();
+
+        localStorage.setItem('cart', JSON.stringify(cart.items));
+
+    }
+};
 
 const ButtonCart = () => {
-
     const dispatch = useDispatch();
-
-    const handlerCardProduct = () => {
-        return (dispatch, getState) => {
-
-            const {
-                product,
-            } = getState();
-            const {
-                nameProduct,
-                selectSize,
-                dataColor,
-            } = product;
-            const {
-                name,
-                price,
-                images,
-            } = dataColor;
-
-            if (selectSize === null) {
-                alert('Размер не выбран, выберите размер');
-            } else {
-                let objData = {
-                    productName: null,
-                    productColor: null,
-                    productSize: null,
-                    price: null,
-                    image: null,
-                };
-
-                objData.productName = nameProduct;
-                objData.productColor = name;
-                objData.price = price;
-                objData.productSize = selectSize;
-                objData.image = images[0];
-
-                dispatch(addProductCart(objData));
-
-                const { cart } = getState();
-
-                localStorage.setItem('cart', JSON.stringify(cart.items));
-
-            }
-        };
-    };
 
     return (
         <button
